b4191213abb8c2fad1f293e14b710f4863414b8e
TonySilva7 <tony.deve7oper@gmail.com>
2024-03-07 22:28:57 -0300

    chore: configure and integrate zod resolvers and zod on react-hook-form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,66 @@
 import React from 'react';
-import {ViewProps} from 'react-native';
+import {Button, Text, TextInput, ViewProps} from 'react-native';
 import {Header} from '../../components/Header';
 import {Container, Title} from './styles';
+import {Controller, useForm} from 'react-hook-form';
+import {zodResolver} from '@hookform/resolvers/zod';
+import * as zod from 'zod';
 
 type HomeProps = ViewProps;
 
+const schema = zod.object({
+  email: zod.string().email('Email é obrigatório'),
+  password: zod.string().min(8, 'Senha deve ter 8 dígitos'),
+});
+
+type IForm = zod.infer<typeof schema>;
+
 export function Home({...rest}: HomeProps) {
+  const {
+    handleSubmit,
+    control,
+    formState: {errors},
+  } = useForm<IForm>({
+    resolver: zodResolver(schema),
+  });
+  const submit = () => {
+    console.log('submit');
+  };
+
+  const onSubmit = handleSubmit(submit);
   return (
     <Container {...rest}>
       <Header />
       <Title>Hello Home</Title>
+      <Controller
+        control={control}
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput
+            placeholder="Email"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="email"
+      />
+      {errors.email && <Text>Email inválido.</Text>}
+
+      <Controller
+        control={control}
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput
+            placeholder="Senha"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            secureTextEntry
+          />
+        )}
+        name="password"
+      />
+      {errors.password && <Text>Senha inválida.</Text>}
+      <Button title="Press me" onPress={onSubmit} />
     </Container>
   );
 }
